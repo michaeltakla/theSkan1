07457ccaa04ef486f08017a45969aec2105470ac
michaeltakla <taklamichael10@gmail.com>
2017-01-16 16:15:18 -0500

    committtt

---
+++ app/src/main/java/com/example/michael/theskan1/MainActivity.java
@@ -0,0 +1,125 @@
+package com.example.michael.theskan1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ShareActionProvider;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends Activity implements View.OnClickListener, AdapterView.OnItemClickListener {
+
+
+    TextView mainTextView;
+    Button mainButton;
+    EditText mainEditText;
+    ListView mainListView;
+    ArrayAdapter mArrayAdapter;
+    ArrayList mNameList = new ArrayList();
+    ShareActionProvider mShareActionProvider;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // 1. Access the TextView defined in layout XML
+        // and then set its text
+        mainTextView = (TextView) findViewById(R.id.main_textview);
+        mainTextView.setText("the sKan!");
+
+        // 2. Access the Button defined in layout XML
+        // and listen for it here
+        mainButton = (Button) findViewById(R.id.main_button);
+        mainButton.setOnClickListener(this);
+
+        // 3. Access the EditText defined in layout XML
+        mainEditText = (EditText) findViewById(R.id.main_edittext);
+
+        // 4. Access the ListView
+        mainListView = (ListView) findViewById(R.id.main_listview);
+
+        // Create an ArrayAdapter for the ListView
+        mArrayAdapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1,
+                mNameList);
+
+        // Set the ListView to use the ArrayAdapter
+        mainListView.setAdapter(mArrayAdapter);
+        // 5. Set this activity to react to list items being pressed
+        mainListView.setOnItemClickListener(this);
+
+        // 6. The text you'd like to share has changed,
+        // and you need to update
+        setShareIntent();
+    }
+
+    @Override
+    public void onClick(View v) {
+        // Test the Button
+        // Take what was typed into the EditText
+        // and use in TextView
+        mainTextView.setText(mainEditText.getText().toString()
+                + " is learning Android development!");
+        // Also add that value to the list shown in the ListView
+        mNameList.add(mainEditText.getText().toString());
+        mArrayAdapter.notifyDataSetChanged();
+
+    }
+    @Override
+    public void onItemClick(AdapterView parent, View view, int position, long id) {
+        // Log the item's position and contents
+        // to the console in Debug
+        Log.d("omg android", position + ": " + mNameList.get(position));
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu.
+        // Adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        // Access the Share Item defined in menu XML
+        MenuItem shareItem = menu.findItem(R.id.menu_item_share);
+
+        // Access the object responsible for
+        // putting together the sharing submenu
+        if (shareItem != null) {
+            mShareActionProvider = (ShareActionProvider)shareItem.getActionProvider();
+        }
+
+        // Create an Intent to share your content
+        setShareIntent();
+
+        return true;
+    }
+
+    private void setShareIntent() {
+
+        if (mShareActionProvider != null) {
+
+            // create an Intent with the contents of the TextView
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Android Development");
+            shareIntent.putExtra(Intent.EXTRA_TEXT, mainTextView.getText());
+
+            // Make sure the provider knows
+            // it should work with that Intent
+            mShareActionProvider.setShareIntent(shareIntent);
+        }
+    }
+
+
+
+}
